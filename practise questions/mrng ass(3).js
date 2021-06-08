be62d84de7fd488287c25340148a95e467d9fdb9
HARSHI245 <85494882+HARSHI245@users.noreply.github.com>
2021-06-08 19:06:16 +0530

    Add files via upload

---
+++ practise questions/mrng ass(3).js
@@ -0,0 +1,11 @@
+<!--external scripting-->
+<!DOCTYPE>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>HARSHI CHOWDARY</title>
+</head>
+<body>
+<script src="alertexternal.js"></script>
+</body>
+</html>
